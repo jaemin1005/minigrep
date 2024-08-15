99777c72c2815c3b653e00096481f7b6e056b530
jaemin1005 <ghkdwoals1005@gmail.com>
2024-08-15 15:10:46 +0900

    Feat | 반복자 어뎁터를 통한 search, build 메서드 수정

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -1,29 +1,34 @@
+use std::env;
 use std::error::Error;
 use std::fs;
-use std::env;
 
 pub struct Config {
     pub query: String,
     pub file_path: String,
-    pub ignore_case: bool
+    pub ignore_case: bool,
 }
 
 impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Not Enough Arguments");
-        }
+    pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        args.next();
 
-        let query = args[1].clone();
-        let file_path = args[2].clone();
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file path"),
+        };
 
         let ignore_case = env::var("IGNORE_CASE").is_ok();
 
-        Ok(Config { 
+        Ok(Config {
             query,
             file_path,
             ignore_case,
-         })
+        })
     }
 }
 
@@ -31,47 +36,37 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
 
     let results = if config.ignore_case {
-      search_case_insentive(&config.query, &contents)
+        search_case_insentive(&config.query, &contents)
     } else {
-      search(&config.query, &contents)
+        search(&config.query, &contents)
     };
 
-
     for line in results {
-      println!("{line}");
+        println!("{line}");
     }
 
     Ok(())
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
+}
+
+pub fn search_case_insentive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
     let mut results = Vec::new();
 
     for line in contents.lines() {
-        if line.contains(query) {
+        if line.to_lowercase().contains(&query) {
             results.push(line);
         }
     }
 
     results
 }
-
-pub fn search_case_insentive<'a>(
-  query: &str,
-  contents: &'a str,
-) -> Vec<&'a str> {
-  let query = query.to_lowercase();
-  let mut results = Vec::new();
-
-  for line in contents.lines() {
-      if line.to_lowercase().contains(&query) {
-          results.push(line);
-      }
-  }
-
-  results
-}
-
 
 #[cfg(test)]
 mod test {
@@ -99,8 +94,8 @@ Pick three.
 Trust me.";
 
         assert_eq!(
-          vec!["Rust:", "Trust me."],
-          search_case_insentive(query, contents)
+            vec!["Rust:", "Trust me."],
+            search_case_insentive(query, contents)
         )
     }
 }
